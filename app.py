b6872e71c3911cca05510f576854ffa147e69433
LDrosdowski <102432857+LDrosdowski@users.noreply.github.com>
2023-05-06 20:42:22 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect, jsonify
+import requests as rs # requisições http
+import json # formatação e manipulação do JSON
+import base64 # conversão base64
+import io # manipulação de entrada e saída
+from matplotlib import pyplot as plt # biblioteca gráfica
+import matplotlib.image as mpimg # biblioteca gráfica
+
+app = Flask(__name__)
+
+# URL da API
+api_url = 'https://localhost:8000'
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    login_data = {
+        'username': username,
+        'password': password
+    }
+
+    response = requests.post(api_url + '/auth', json=login_data)
+    data = response.json()
+
+    if 'token' in data:
+        return redirect('/tasks?token=' + data['token'])
+    else:
+        error = data.get('error', 'Erro de autenticação')
+        return render_template('login.html', error=error)
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+
+    signup_data = {
+        'username': username,
+        'password': password
+    }
+
+    response = requests.post(api_url + '/signup', json=signup_data)
+    data = response.json()
+
+    if 'token' in data:
+        return redirect('/tasks?token=' + data['token'])
+    else:
+        error = data.get('error', 'Erro ao criar conta')
+        return render_template('signup.html', error=error)
+
+@app.route('/tasks')
+def tasks():
+    token = request.args.get('token')
+
+    headers = {
+        'Authorization': 'Bearer ' + token
+    }
+
+    response = requests.get(api_url + '/tasks', headers=headers)
+    data = response.json()
+
+    tasks = data.get('tasks', [])
+
+    return render_template('tasks.html', tasks=tasks)
+
+if __name__ == '__main__':
+    app.run()
