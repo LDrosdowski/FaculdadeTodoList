3e0d49ee4218837773c09442bb113753ba4824e3
LDrosdowski <102432857+LDrosdowski@users.noreply.github.com>
2023-05-10 21:28:39 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request, redirect, session
 import requests as rs
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 # URL da API
 api_url = 'https://todolist-api.edsonmelo.com.br'
 
-@app.route('/login')
+@app.route('/')
 def index():
+    return redirect('login')
+
+@app.route('/login', methods=['GET'])
+def loginPage():
     return render_template('login.html')
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
@@ -26,13 +32,13 @@ def login():
 
     response = rs.post(api_url + '/api/user/login/', json=login_data, headers=headers)
     status = response.json()
-    token = status['token']
-    session['token'] = token
 
     if 'token' in status:
-        redirect ('tasksPage')
+        token = status['token']
+        session['token'] = token
+        return redirect ('tasks/list')
     else:
-        error = status.get('error', 'Erro de autenticação')
+        error = status.get('error', 'Incorrect username and/or password')
         return render_template('login.html', error=error)
 
 @app.route('/signup', methods=['GET'])
@@ -68,24 +74,94 @@ def signup():
         error = status.get('error', 'User Already Exists')
         return render_template('signup.html', error=error)
 
-@app.route('/tasks/list', methods=['GET'])
+@app.route('/tasks/list')
 def tasksPage():
     return render_template('list.html')
 
-@app.route('/tasks')
+@app.route('/tasks/list', methods=['POST'])
 def newTask():
-    token = request.args.get('token')
+    taskName = request.form['taskName']
 
-    headers = {
-        'Authorization': 'Bearer ' + token
+    signup_data = {
+        'name': taskName
     }
 
-    response = rs.get(api_url + '/tasks', headers=headers)
-    data = response.json()
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': session['token']
+    }
 
-    tasks = data.get('tasks', [])
+    response = rs.post(api_url + '/api/task/new/', json=signup_data, headers=headers)
+    status = response.json()
+    message = status['message']
 
-    return render_template('tasks.html', tasks=tasks)
+    if message == "Task Successfully Added":
+        success = status.get('success', 'Task Successfully Added')
+        return render_template('list.html', success=success)
+    else:
+        error = status.get('error', 'Could Not Add Task')
+        return render_template('list.html', error=error)
+
+@app.route('/delete')
+def deleteTaskPage():
+    return render_template('delete.html')
+
+@app.route('/delete', methods=['DELETE'])
+def deleteTask():
+    taskId = request.form['taskId']
+    
+    signup_data = {
+         'taskId': taskId
+     }
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': session['token']
+    }
+
+    response = rs.delete(api_url + '/api/task/delete/', json=taskId, headers=headers)
+    status = response.json()
+    message = status['message']
+    print(message)
+
+    if message == "Task deleted Successfully":
+        success = status.get('success', 'Task deleted Successfully')
+        return render_template('delete.html', success=success)
+    else:
+        error = status.get('error', 'Task not exist')
+        return render_template('delete.html', error=error)
+
+@app.route('/edit')
+def editTaskPage():
+    return render_template('edit.html')
+
+@app.route('/edit', methods=['PUT'])
+def editTask():
+    taskId = request.form['taskId']
+    name = request.form['name']
+    realized = request.form['realized']
+    
+    signup_data = {
+        'taskId': taskId,
+        'name': name,
+        'realized': realized
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': session['token']
+    }
+
+    response = rs.put(api_url + '/api/task/update/', json=signup_data, headers=headers)
+    status = response.json()
+    message = status['message']
+
+    if message == "Task Successfully Updated":
+        success = status.get('success', 'Task Successfully Updated')
+        return render_template('edit.html', success=success)
+    else:
+        error = status.get('error', 'Task(s) not found')
+        return render_template('edit.html', error=error)
 
 if __name__ == '__main__':
     app.run()
